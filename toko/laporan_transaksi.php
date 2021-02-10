b653e09cf5c566f3fc8e669e4f9664571f9fcabe
ibnu.rohan@uii.ac.id <ibnu.rohan@uii.ac.id>
2021-02-10 14:40:05 +0700

    Tambah fitur filter laporan untuk kategori dan barang

--- toko/laporan_transaksi.php
+++ toko/laporan_transaksi.php
@@ -1,27 +1,30 @@
 <?php
-include '../load_files.php';
-include_once '../plugins/dompdf/autoload.inc.php';
+	include '../load_files.php';
+	include_once '../plugins/dompdf/autoload.inc.php';
 
-$tanggal_awal   = (!empty($_POST['tanggal_awal'])) ? $_POST['tanggal_awal'] : "0000-00-00";
-$tanggal_akhir  = (!empty($_POST['tanggal_akhir'])) ? $_POST['tanggal_akhir'] : date("Y-m-d");
-$id_kategori    = (!empty($_POST['id_kategori'])) ? $_POST['id_kategori'] : "";
-$id_barang      = (!empty($_POST['id_barang'])) ? $_POST['id_barang'] : "";
+	$tanggal_awal   = (!empty($_POST['tanggal_awal'])) ? $_POST['tanggal_awal'] : "0000-00-00";
+	$tanggal_akhir  = (!empty($_POST['tanggal_akhir'])) ? $_POST['tanggal_akhir'] : date("Y-m-d");
+	$id_kategori    = ($_POST['id_kategori']) ? $_POST['id_kategori'] : NULL;
+	$id_barang      = ($_POST['id_barang']) ? $_POST['id_barang'] : NULL;
 
-$sql = "SELECT * FROM `data_transaksi` LEFT JOIN `data_pelanggan` ON data_transaksi.id_pelanggan = data_pelanggan.id_pelanggan WHERE ((data_transaksi.id_toko = '$_SESSION[id]') AND (data_transaksi.tgl_transaksi >= '$tanggal_awal 00:00:00') AND (data_transaksi.tgl_transaksi <= '$tanggal_akhir 23:59:00')) ORDER BY data_transaksi.id_transaksi DESC";
+	$sql = "SELECT * FROM `data_transaksi` LEFT JOIN `data_pelanggan` ON data_transaksi.id_pelanggan = data_pelanggan.id_pelanggan LEFT JOIN `data_transaksi_detail` ON data_transaksi.id_transaksi = data_transaksi_detail.id_transaksi LEFT JOIN `data_barang` ON data_transaksi_detail.id_barang = data_barang.id_barang LEFT JOIN `data_kategori` ON data_kategori.id_kategori = data_barang.id_kategori WHERE ((data_transaksi.id_toko = '$_SESSION[id]') AND (data_transaksi.tgl_transaksi >= '$tanggal_awal 00:00:00') AND (data_transaksi.tgl_transaksi <= '$tanggal_akhir 23:59:00')) ";
+	$sql .= ($id_barang) ? "AND (data_barang.id_barang = '$id_barang') " : " " ;
+	$sql .= ($id_kategori) ? "AND (data_barang.id_kategori = '$id_kategori') " : " " ;
+	$sql .= "ORDER BY data_transaksi.id_transaksi DESC ";
 
-$transaksiAll = mysqli_query($koneksi, $sql) or die($koneksi);
+	$transaksiAll = mysqli_query($koneksi, $sql) or die($koneksi);
 
-$jumlahHarga = 0;
+	$jumlahHarga = 0;
 
-$i = 1;
+	$i = 1;
 
-// reference the Dompdf namespace
-use Dompdf\Dompdf;
+	// reference the Dompdf namespace
+	use Dompdf\Dompdf;
 
-// instantiate and use the dompdf class
-$dompdf = new Dompdf();
+	// instantiate and use the dompdf class
+	$dompdf = new Dompdf();
 
-ob_start();
+	ob_start();
 ?>
 
 <!DOCTYPE html>
@@ -39,7 +42,7 @@ ob_start();
 <body>
 	<p class="text-dark">
 		<h2 class="text-center">Laporan Transaksi Tanggal <?php echo format(date('Y-m-d'), 'date'); ?></h2>
-		<?php if ($tanggal_awal == "1978-01-01" and $tanggal_akhir == date("Y-m-d")) : ?>
+		<?php if (($tanggal_awal == "1978-01-01" || $tanggal_awal == "0000-00-00" ) and $tanggal_akhir == date("Y-m-d")) : ?>
 			<table>
 				<tbody>
 					<tr>
@@ -73,6 +76,25 @@ ob_start();
 				</tbody>
 			</table>
 		<?php endif ?>
+		<table>
+			<tbody>
+				<?php if ($id_kategori): ?>
+					<tr>
+						<td>Kategori</td>
+						<td>:</td>
+						<td><?= mysqli_fetch_assoc(mysqli_query($koneksi, "SELECT * FROM `data_kategori` WHERE `id_kategori` = '$id_kategori' "))['nama_kategori'] ?></td>
+					</tr>
+				<?php endif ?>
+				<?php if ($id_barang): ?>
+					<tr>
+						<td>Barang</td>
+						<td>:</td>
+						<td><?= mysqli_fetch_assoc(mysqli_query($koneksi, "SELECT * FROM `data_barang` WHERE `id_barang` = '$id_barang' "))['nama_barang'] ?></td>
+					</tr>
+				<?php endif ?>
+			</tbody>
+		</table>
+		
 	</p>
 	<table class="table table-bordered table-striped table-hover">
 		<thead>
@@ -84,30 +106,34 @@ ob_start();
 				<th>Total Harga</th>
 				<th>Diantarkan</th>
 				<th>Status Transaksi</th>
-				<th>Rating</th>
+				<!-- <th>Rating</th> -->
 			</tr>
 		</thead>
 		<tbody>
-			<?php while ($transaksi = mysqli_fetch_array($transaksiAll, MYSQLI_BOTH)) : ?>
-				<?php
-				$sql = "SELECT * FROM `data_transaksi_detail` LEFT JOIN `data_barang` ON data_transaksi_detail.id_barang = data_barang.id_barang LEFT JOIN `data_kategori` ON data_barang.id_kategori = data_kategori.id_kategori WHERE (data_transaksi_detail.id_transaksi = '$transaksi[id_transaksi]') AND (data_barang.id_kategori LIKE '%$id_kategori') AND (data_transaksi_detail.id_barang LIKE '%$id_barang')";
-				$detailAll = mysqli_query($koneksi, $sql) or die($koneksi);
-				foreach ($detailAll as $key => $value) {
-					$jumlahHarga += $value['harga_sewa'] * $value['jumlah_barang_sewa'] * $transaksi['jumlah_hari'];
-				}
-				?>
-				<tr>
-					<th><?= $i ?></th>
-					<th><?= $transaksi['no_transaksi'] ?></th>
-					<th><?= $transaksi['tgl_transaksi'] ?></th>
-					<th><?= $transaksi['nama_pelanggan'] ?></th>
-					<th><?= format($jumlahHarga, 'currency') ?></th>
-					<th><?= setBadges($transaksi['diantarkan']) ?></th>
-					<th><?= setBadges($transaksi['status_transaksi']) ?></th>
-					<th><?= showRating($transaksi['rating'], 11) ?></th>
-				</tr>
-				<?php $i++; ?>
-			<?php endwhile ?>
+			<?php if (mysqli_num_rows($transaksiAll) <= 0): ?>
+				<tr> <td colspan="7"> <p class="text-center">Tidak ada data...</p> </td> </tr>
+			<?php else: ?>
+				<?php while ($transaksi = mysqli_fetch_array($transaksiAll, MYSQLI_BOTH)) : ?>
+					<?php
+						$sql = "SELECT * FROM `data_transaksi_detail` LEFT JOIN `data_barang` ON data_transaksi_detail.id_barang = data_barang.id_barang LEFT JOIN `data_kategori` ON data_barang.id_kategori = data_kategori.id_kategori WHERE (data_transaksi_detail.id_transaksi = '$transaksi[id_transaksi]') AND (data_barang.id_kategori LIKE '%$id_kategori') AND (data_transaksi_detail.id_barang LIKE '%$id_barang') ";
+						$detailAll = mysqli_query($koneksi, $sql) or die($koneksi);
+						foreach ($detailAll as $key => $value) {
+							$jumlahHarga += $value['harga_sewa'] * $value['jumlah_barang_sewa'] * $transaksi['jumlah_hari'];
+						}
+					?>
+					<tr>
+						<th><?= $i ?></th>
+						<th><?= $transaksi['no_transaksi'] ?></th>
+						<th><?= $transaksi['tgl_transaksi'] ?></th>
+						<th><?= $transaksi['nama_pelanggan'] ?></th>
+						<th><?= format($jumlahHarga, 'currency') ?></th>
+						<th><?= setBadges($transaksi['diantarkan']) ?></th>
+						<th><?= setBadges($transaksi['status_transaksi']) ?></th>
+						<!-- <th><?php //showRating($transaksi['rating'], 11) ?></th> -->
+					</tr>
+					<?php $i++; ?>
+				<?php endwhile ?>
+			<?php endif ?>
 		</tbody>
 	</table>
 </body>
@@ -116,20 +142,20 @@ ob_start();
 
 <?php
 
-$html = ob_get_contents(); //Proses untuk mengambil hasil dari OB..
-ob_end_clean();
+	$html = ob_get_contents(); //Proses untuk mengambil hasil dari OB..
+	ob_end_clean();
 
-// (Optional) Setup the paper size and orientation
-$dompdf->setPaper('A4', 'landscape');
-// $dompdf->setPaper(array(0, 0, 550, 300));
+	// (Optional) Setup the paper size and orientation
+	$dompdf->setPaper('A4', 'landscape');
+	// $dompdf->setPaper(array(0, 0, 550, 300));
 
-$dompdf->loadHtml(utf8_encode($html));
+	$dompdf->loadHtml(utf8_encode($html));
 
-// Render the HTML as PDF
-$dompdf->render();
+	// Render the HTML as PDF
+	$dompdf->render();
 
-// Output the generated PDF to Browser
-$dompdf->stream("Laporan_Transaksi_" . date('Y-m-d') . ".pdf", array("Attachment" => 0));
+	// Output the generated PDF to Browser
+	$dompdf->stream("Laporan_Transaksi_" . date('Y-m-d') . ".pdf", array("Attachment" => 0));
 
-// exit(0);
+	// exit(0);
 ?>
